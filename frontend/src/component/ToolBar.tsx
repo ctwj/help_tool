c4e524dc9b8334ad7e9acc5231bedabf937bd8cc
ctwj <908504609@qq.com>
2023-12-12 17:52:11 +0800

    update: 跟新应用导航

--- frontend/src/component/ToolBar.tsx
+++ frontend/src/component/ToolBar.tsx
@@ -1,7 +1,35 @@
-import { Link } from 'react-router-dom'
-import Box from '@mui/material/Box';
+import { useNavigate, useLocation } from 'react-router-dom'
+
+import { Box, Toolbar, AppBar, Typography, 
+    IconButton, Stack, createTheme
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 export default function ToolBar() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    
+
+    const toRouter = (router:string) => {
+        navigate(router);
+    }
+
+    const MenuItem = (props: {name: string, to:string}) => {
+        const {name, to} = props
+        const isActive = location.pathname === to;
+
+        return (
+            <Typography 
+                variant="h6" 
+                color={isActive ? 'secondary' : 'inherit'}
+                component="div" 
+                onClick={() => toRouter(to)}>
+                {name}
+            </Typography>
+        )
+    }
+
     return (
         <Box sx={{
             display: 'flex',
@@ -11,8 +39,19 @@ export default function ToolBar() {
             flexShrink: '0',
             width: '100%',
         }}>
-            <Link to="/">Home</Link>
-            <Link to="/other">Other</Link>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <MenuIcon />
+                    </IconButton>
+
+                    <Stack direction="row" spacing={1}>
+                        <MenuItem name="Home" to="/" />
+                        <MenuItem name="Other" to="/other" />
+                    </Stack>
+                    
+                </Toolbar>
+            </AppBar>
         </Box>
     )
 }
